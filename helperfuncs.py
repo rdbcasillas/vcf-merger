0d9892a4fa1e572bb66383a7f0d0dcd88d6cd9fc
rdbcasillas <rdbcasillas11@gmail.com>
2022-02-10 01:43:45 +0530

    first commit

---
+++ helperfuncs.py
@@ -0,0 +1,47 @@
+from re import match
+def getAllPositions(file1, file2):
+    locationArray = []
+    with open(file1, 'rt') as vcf1:
+        for line1 in vcf1:
+            if not match('^#', line1):
+                columns = line1.split('\t')
+                #chromosome = columns[0]
+                position = columns[1]
+                locationArray.append(position)
+                
+    with open(file2, 'rt') as vcf2:
+        for line2 in vcf2:
+            if not match('^#', line2):
+                columns = line2.split('\t')
+                #chromosome = columns[0]
+                position = columns[1]
+                locationArray.append(position)
+    return list(set(locationArray))
+
+
+def getLinesWithLocation(chromloc, vcf):
+    myline = []
+    with open(vcf, 'rt') as vcflines:
+        for line in vcflines:
+            if chromloc in line:
+                myline.append(line)
+    return myline
+
+
+def getFormatTags(format1, format2, tool1, tool2):
+    formatTags = []
+    format1 = format1.split(':')
+    format2 = format2.split(':')
+    for i in range(len(format1)):
+        if format1[i] in format2:
+            formatTags.append(tool1+ "_" + format1[i])
+        else:
+            formatTags.append(format1[i])
+    
+    for i in range(len(format2)):
+        if format2[i] in format1:
+            formatTags.append(tool2 + "_" + format2[i])
+        else:
+            formatTags.append(format2[i])
+
+    return ':'.join(formatTags)
---
+++ vcfmerger.py
@@ -0,0 +1,69 @@
+#!/Users/rdbcasillas/miniconda3/bin/python
+from argparse import ArgumentParser
+import csv
+from helperfuncs import getLinesWithLocation,getFormatTags,getAllPositions
+
+parser = ArgumentParser()
+requiredNamed = parser.add_argument_group('Required Arguments')
+requiredNamed.add_argument("-v1", "--vcf1", dest = "vcf1",  help="First VCF", required=True)
+requiredNamed.add_argument("-v2", "--vcf2", dest = "vcf2", help="Second VCF", required=True)
+requiredNamed.add_argument("-o", "--output",dest ="output", help="Output merged file",  required=True)
+requiredNamed.add_argument("-t", "--tools",dest = "tools", help="Comma separated list of tools to be merged" , required=True)
+
+args = parser.parse_args()
+
+tool1 = args.tools.split(',')[0]
+tool2 = args.tools.split(',')[1]
+
+
+chromLocations = set(getAllPositions(args.vcf1,args.vcf2))
+
+
+def mergeVCFs(file1, file2):
+    with open(args.output, 'a') as out:
+        # out.write('##fileformat=VCFv4.1\n')
+        # out.write('##source=newmerge.py\n')
+        # out.write('\t'.join(['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT'])+'\n')
+
+        for loc in chromLocations:
+            tmp1 =  getLinesWithLocation(loc, file1)
+            tmp2 =  getLinesWithLocation(loc, file2)
+
+            if len(tmp1) > 0 and len(tmp2) > 0:
+                newlist = []
+                vcf1cols = tmp1[0].split('\t')
+                vcf2cols = tmp2[0].split('\t')
+                newinfotags = vcf1cols[7] + ";" + vcf2cols[7] + ";calledBy=" + tool1 + "+" + tool2
+                newformattags = getFormatTags(vcf1cols[8], vcf2cols[8], tool1, tool2)
+                newformattagvals = vcf1cols[9].strip('\n') + ":" + vcf2cols[9].strip('\n')
+
+                newlist.extend((vcf1cols[0], vcf1cols[1], vcf1cols[2], vcf1cols[3],vcf1cols[4],vcf2cols[5],vcf2cols[6],newinfotags,newformattags, newformattagvals))
+                commonline = '\t'.join(newlist)
+                out.write(commonline + "\n")
+            elif len(tmp1) > 0 and len(tmp2) == 0:
+                tmpcols = tmp1[0].split('\t')
+                tmpcols[7] = tmpcols[7]+";"+ tool1 + "_Only"
+                firstToolLine = '\t'.join(tmpcols)
+                out.write(firstToolLine + "\n")
+            else: 
+                tmpcols2 = tmp2[0].split('\t')
+                tmpcols2[7] = tmpcols2[7]+";"+ tool2 + "_Only"
+                secondToolLine = '\t'.join(tmpcols2)
+                out.write(secondToolLine)
+
+def sortOutput():
+    with open(args.output, 'rt') as vcf:
+        with open(args.output + ".sorted", 'a') as out:
+            out.write('##fileformat=VCFv4.1\n')
+            out.write('##source=newmerge.py\n')
+            out.write('\t'.join(['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT'])+'\n')
+            reader = csv.reader(vcf, delimiter='\t')
+            #out.writerows
+            for line in sorted(reader, key=lambda row: (int(row[0][3:]), int(row[1]))):
+                out.write(line)
+def main():
+   mergeVCFs(args.vcf1,args.vcf2) 
+   sortOutput()
+
+if __name__ == "__main__":
+    main()
